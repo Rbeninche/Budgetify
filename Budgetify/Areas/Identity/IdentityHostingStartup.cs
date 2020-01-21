50bf88dc0c39072eaa5a2cc14dc590be5fca15f1
Reginald Beninche <rbeninche@gmail.com>
2020-01-21 01:02:40 -0500

    Add project files.

---
+++ Budgetify/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Budgetify.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Budgetify.Areas.Identity.IdentityHostingStartup))]
+namespace Budgetify.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Budgetify/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Budgetify.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
